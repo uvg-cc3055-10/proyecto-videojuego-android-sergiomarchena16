19292802582cc3ab4effe6946e18e1510bcc544c
Sergio Marchena <mar16387@uvg.edu.gt>
2018-05-08 23:56:49 -0600

    musica inicio
    falta componer las animaciones

---
+++ SaveLucas/New Unity Project/Assets/Scripts/Laser1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser1 : MonoBehaviour {
+
+	float lifeTime=3;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.right * 25 * Time.deltaTime);
+
+		lifeTime += Time.deltaTime * -1;
+		if (lifeTime < 0) {
+			Destroy (this.gameObject);
+		}
+	}
+}
--- SaveLucas/New Unity Project/Assets/Scripts/Lucas.cs
+++ SaveLucas/New Unity Project/Assets/Scripts/Lucas.cs
@@ -7,35 +7,49 @@ public class Lucas : MonoBehaviour {
 
 	Rigidbody2D rb2d;
 	SpriteRenderer sr;
-	private float speed = 5f;
+	private float speed = 6.5f;
 	private float jumpForce = 250f;
 	private bool facingRight = true;
 	Animator anim;
-//	AudioSource aux;
+	private bool salto, shoot;
+
+	public GameObject laser1;
+
+
+	AudioSource aux;
 
 	// Use this for initialization
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D>();
 		sr = GetComponent<SpriteRenderer>();
 		anim = GetComponent<Animator>();
-//		aux = GetComponent<AudioSource>();
+		anim.SetBool ("salto", false);
+		aux = GetComponent<AudioSource>();
+		aux.Play();
 	}
 
 	// Update is called once per frame
 	void Update () {
+		
 		float move = Input.GetAxis("Horizontal");
 		if (move != 0) {
 			rb2d.transform.Translate(new Vector3(1, 0, 0) * move * speed * Time.deltaTime);
 //			cam.transform.position = new Vector3(rb2d.transform.position.x, cam.transform.position.y, cam.transform.position.z);
 			facingRight = move > 0;
 		}
-
 		sr.flipX = !facingRight;
 		anim.SetFloat("speed", Mathf.Abs(move));
 
 		if (Input.GetButtonDown("Jump")) {
 			rb2d.AddForce(Vector2.up*jumpForce);
-//			aux.Play();
+			anim.SetBool ("salto", true);
+		}
+
+		if (Input.GetButtonDown("Fire1")){
+			//Debug.Log("Bang");
+			anim.SetBool ("shoot", true);
+			Instantiate(laser1, transform.position, Quaternion.identity);
+
 		}
 
 	}

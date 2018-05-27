846d6667458a7ba9e3af539886427e2c270ece83
Sergio Marchena <mar16387@uvg.edu.gt>
2018-05-26 18:46:51 -0600

    only menu left
    only menu left

---
+++ SaveLucas/New Unity Project/Assets/Scripts/BalaE3.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BalaE3 : MonoBehaviour {
+	float lifeTime = 4;
+
+	// Use this for initialization
+	void Start (){
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.right * 13 * Time.deltaTime);
+		lifeTime += Time.deltaTime * -1;
+		if (lifeTime < 0) {
+			Destroy (this.gameObject);
+		}
+	}
+}
+
